7409f88c4f944057a86ae184edc25d1d4da0ee83
Chris Dimoulis <christopherjamesd@gmail.com>
2016-02-07 17:10:33 -0600

    [ADDED] Test models

---
+++ api/controllers/ObjController.js
@@ -0,0 +1,11 @@
+/**
+ * ObjController
+ *
+ * @description :: Server-side logic for managing objs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
