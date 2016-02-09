79a2d0aefcb1079ff741b145780b382b676f350c
Chris Dimoulis <christopherjamesd@gmail.com>
2016-02-09 16:49:53 -0600

    [ADDED] Test controller function

--- api/controllers/ObjController.js
+++ api/controllers/ObjController.js
@@ -7,5 +7,27 @@
 
 module.exports = {
 	
+  test: function (req,res) {
+    console.log('\n\nTEST FUNCTION!\n\n');
+    template = "\
+    <html>\
+      <head>\
+        <script src='//ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js'></script>\
+      </head>\
+      <body>\
+        Test Worked\
+      </body>\
+    </html>\
+    ";
+    return res.send(template);
+  },
+
+
+  getData: function (req,res) {
+    data = {one: 'Data 1', two: 'Data 2', three: "Data 3"};
+    console.log("\n\nSending ",data,"\n\n");
+    return res.json(data);
+  }
+
 };
 
