f692601f34180652ffb3d38d7f432ab02828a29a
Chris Dimoulis <christopherjamesd@gmail.com>
2016-02-25 16:28:11 -0600

    [ADDED] data-view-name for understanding

--- assets/js/application/pages/index.js
+++ assets/js/application/pages/index.js
@@ -1,4 +1,15 @@
 App.Page.extend({
   name: 'pages/index',
+  tagName: 'form',
+  attributes:{
+    'class': 'party'
+  },
+  init_functions: [
+    'setup'
+  ],
+
+  setup: function(){
+
+  },
 
 });
--- assets/js/libs/view.js
+++ assets/js/libs/view.js
@@ -8,15 +8,16 @@ Backbone.View = Backbone.View.extend({
 
     this._processData(options.hash || {});
     this._processInitFunctions();
+    this._processAttributes();
   },
 
 
-  _processData: function(hash){
+  _processData: function(hash) {
     var val, 
     _this = this,
     data = hash.data || {};
 
-    _.each(this.data_source, function(source){
+    _.each(this.data_source, function(source) {
 
       if (source.required){
         if (_.isUndefined(data[source.key]) || _.isNull(data[source.key])){
@@ -35,13 +36,13 @@ Backbone.View = Backbone.View.extend({
 
   },
 
-  _processInitFunctions: function(){
+  _processInitFunctions: function() {
     var _this = this;
 
     if (!_.isUndefined(this.init_functions) && !_.isNull(this.init_functions) &&
         !_.isEmpty(this.init_functions)){
 
-      _.each(this.init_functions, function(func){
+      _.each(this.init_functions, function(func) {
 
         if (!_this[func] && !_.isFunction(_this[func])){
           throw 'View '+this.name+' does not contain function: '+func;
@@ -52,6 +53,10 @@ Backbone.View = Backbone.View.extend({
         _this[func].call(this);
       });
     }
+  },
+
+  _processAttributes: function() {
+    this.$el.attr('data-view-name',this.name);
   },
 
   render: function() {
